77946f147770b1d1283f111eba313d0e8d115ca7
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-03-15 16:33:49 +0530

    Sunny Land Game Initiated... Player Movement Done... Player Jump done... Animations are done....

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+    public float jumpForce = 50f;
+    Rigidbody2D rb;
+    Vector2 movement;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        animator.SetFloat("isRunning",movement.sqrMagnitude);
+        
+            
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * playerSpeed * Time.fixedDeltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0f,movement.y * jumpForce),ForceMode2D.Impulse);
+            animator.SetBool("isJump",true);
+        }
+    }
+}
